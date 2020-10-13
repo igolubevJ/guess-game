2997b5b6919afba5b1e91a561663c96f59e561ca
igolubev <igolubevapp@gmail.com>
2020-10-13 21:32:28 +0300

    Add loop and error handler

--- src/main.rs
+++ src/main.rs
@@ -9,20 +9,27 @@ fn main() {
     let secret_number = rand::thread_rng().gen_range(1, 101);
     println!("Секретное число равно {}", secret_number);
 
-    println!("Пожадуйста, введите свою догадку.");
+    loop {
+        println!("Пожадуйста, введите свою догадку.");
+        
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess)
+            .expect("Не получилось прочитать строку");
 
-    let mut guess = String::new();
-    io::stdin().read_line(&mut guess)
-        .expect("Не получилось прочитать строку");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-    let guess: u32 = guess.trim().parse()
-        .expect("Пожалуйста, набирите число!");
+        println!("Вы загадали: {}", guess);
 
-    println!("Вы загадали: {}", guess);
-
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Слишком малое число!"),
-        Ordering::Greater => println!("Слишком большое число!"),
-        Ordering::Equal => println!("Вы выйграли!"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Слишком малое число!"),
+            Ordering::Greater => println!("Слишком большое число!"),
+            Ordering::Equal => {
+                println!("Вы выйграли!");
+                break;
+            },
+        }
     }
 }
